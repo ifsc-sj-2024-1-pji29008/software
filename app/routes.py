7b9b007ff36521a2d55fbc83a22c6656aa030ca1
Mike <mikeicd@gmail.com>
2024-07-16 12:36:40 -0300

    Refactor Flask route to use render_template for home page

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 # Define your Flask routes here
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, render_template
 from loguru import logger
 
-bp = Blueprint('main', __name__)
+bp = Blueprint("main", __name__)
 
-@bp.route('/')
+
+@bp.route("/")
 def home():
-    logger.info('Home page accessed')
-    return jsonify({"message": "Hello, World!"})
+    logger.info("Home page accessed")
+    return render_template("index.html")
