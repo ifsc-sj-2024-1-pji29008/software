3ff6b008a7e5b8fba6af821799b27ee0e060a075
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2024-07-23 18:14:30 -0300

    chore: Add shebang for easy execution. Added custom host and port to the flask object.

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from app import create_app
 
 # Cria a instância da aplicação Flask
@@ -5,4 +7,4 @@ app = create_app()
 
 if __name__ == '__main__':
     # Inicia o servidor Flask
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host='0.0.0.0')
