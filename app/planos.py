229f21a9d240f5bbb34dce66f37ef2aee80e55c0
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2024-07-24 19:43:59 -0300

    chore: the rotines now use the new sensor class to verify the sensors.

--- app/planos.py
+++ app/planos.py
@@ -1,7 +1,7 @@
 from flask import current_app
 
 from audioop import add
-from .lib.jpmsb import onewire
+from .lib.jpmsb import sensor
 from .database import db
 from .models import Sensor, StatusPlano
 
@@ -12,35 +12,32 @@ from loguru import logger
 # Plano de testes de temperatura
 def test_temp(app_context):
     app_context.push()
-    ow = onewire('app/temp/sys/bus/w1/devices')
+    ow_sensor = sensor('app/temp/sys/bus/w1/devices')
     sensors = {}
-    # Garantir que o sensor foi encontrado
-    barramentos = ow.list_w1_buses()
-    # Coletar dados do sensor
-    for i, bar in enumerate(barramentos):
-        logger.debug(bar)
-        sensors = ow.list_sensors(i+1)
-        for j, sensor in enumerate(sensors):            
-            logger.debug(ow.get_address(i+1, j+1))
-            logger.debug(ow.get_temperature(i+1, j+1))
-            temperature = ow.get_temperature(i+1, j+1)
-            address = ow.get_address(i+1, j+1)
+
+    # Verifica cada sensor registrado
+    sensors_addresses = ow_sensor.list_sensors()
+    for index, sensor_address in enumerate(sensors_addresses):
+            logger.debug(sensor_address)
+            logger.debug(ow_sensor.get_temperature(index+1))
+
+            temperature = ow_sensor.get_temperature(index+1)
+            address = sensor_address
+
             # Veredito
             if temperature > 40:
                 verdict = "fail"
             else:
                 verdict = "pass"
-            
+
             # Salvar o resultado no banco de dados
             sensor_data = Sensor(serialNumber=address, temperature=temperature, verdict=verdict)
             db.session.add(sensor_data)
-            
+
             # Encontra status do plano 3 e altera para complete
             status = StatusPlano.query.filter_by(plano='plano3').first()
             status.status = 'complete'
             db.session.add(status)          
-            
+
             # Salva no banco de dados
             db.session.commit()
-    
-    
