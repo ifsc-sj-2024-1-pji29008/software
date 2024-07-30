dfca6237c283d9db953918a3abb1be45c54810fc
Mike <mikeicd@gmail.com>
2024-07-30 10:43:25 -0300

    Aprimorado plano de temperatura

--- app/planos.py
+++ app/planos.py
@@ -9,31 +9,46 @@ def plano_temperatura(ow_sensor):
     # Verifica cada sensor registrado
     sensors_addresses = ow_sensor.list_sensors()
     for index, sensor_address in enumerate(sensors_addresses):
-        logger.debug(sensor_address)
-        logger.debug(ow_sensor.get_temperature(index + 1))
-
-        temperature = ow_sensor.get_temperature(index + 1)
-        address = sensor_address
-
+        try:
+            temperature = ow_sensor.get_temperature(index + 1)
+            logger.debug(sensor_address)
+            logger.debug(temperature)
+        except Exception as e:
+            logger.warning(f"Erro ao obter temperatura do sensor {sensor_address}")
+            logger.debug(e)
+            continue
+            
         # Veredito
         if temperature > 40:
             verdict = "fail"
         else:
             verdict = "pass"
-
-        # Salvar o resultado no banco de dados
-        sensor_data = Sensor(
-            serialNumber=address, temperature=temperature, verdict=verdict
-        )
-        db.session.add(sensor_data)
-
-        # Encontra status do plano e altera para complete
-        status = StatusPlano.query.filter_by(plano="temperatura").first()
-        status.status = "complete"
-        db.session.add(status)
+        
+        # Verifica se o sensor já foi registrado
+        sensor_data = Sensor.query.filter_by(serialNumber=sensor_address).first()
+        if sensor_data:
+            sensor_data.temperature = temperature
+            sensor_data.verdict = verdict
+            db.session.add(sensor_data)
+            continue
+        else:
+            sensor_data = Sensor(
+                serialNumber=sensor_address, temperature=temperature, verdict=verdict
+            )
+            db.session.add(sensor_data)
+        
+    # Encontra status do plano e altera para complete
+    status = StatusPlano.query.filter_by(plano="temperatura").first()
+    status.status = "complete"
+    db.session.add(status)
 
     # Salva no banco de dados
-    db.session.commit()
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        logger.error("Erro ao salvar no banco de dados")
+        logger.debug(e)
 
 
 def plano_curto(ow_sensor):
