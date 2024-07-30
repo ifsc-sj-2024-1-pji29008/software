d9f27592426ea31602492ef865202152cafeb7a1
rafafonsek <irla.r@aluno.ifsc.edu.com.br>
2024-07-30 12:47:17 -0300

    Iniciando o banco de dados relacional

--- app/models.py
+++ app/models.py
@@ -1,23 +1,82 @@
 # Define your database models here
-
+from sqlalchemy import Column, ForeignKey, Integer, String
 from datetime import datetime
 from . import db
 
-class SensorData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    temperature = db.Column(db.Float, nullable=False)
-    humidity = db.Column(db.Float, nullable=False)
-    verdict = db.Column(db.String(50), nullable=False)
-    type = db.Column(db.String(50), nullable=False)
-    dateTime = db.Column(db.DateTime, default=datetime.now)
-    idSerial = db.Column(db.String(50), nullable=False)
-    serialNumber = db.Column(db.String(50), nullable=False)    
 class Sensor(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    temperature = db.Column(db.Float, nullable=False)
+    __tablename__ = 'sensor'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    serialNumber = db.Column(db.String(50), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
     verdict = db.Column(db.String(50), nullable=False)
-    serialNumber = db.Column(db.String(50), nullable=False) 
+    description = db.Column(db.String(50), nullable=True)
+    data = db.relationship('SensorData',backref='sensor')
+    
+    def __repr__(self):
+        return f'Sensor {self.id}'
+    
+    # Consultando todas as informações de um sensor
+    @classmethod
+    def find_sensor(cls, session, id):
+        sensor = session.query(cls).filter_by(id=id).all()
+        for aux in sensor:
+            print(sensor.id, sensor.serialNumber, sensor.verdict, sensor.type, sensor.description)
+        return sensor
+
+    # Adicionando um novo sensor
+    @classmethod
+    def add_sensor(cls, serialNumber, type, verdict, description=None):
+        new_sensor = cls(serialNumber=serialNumber, type=type, verdict=verdict, description=description)
+        db.session.add(new_sensor)
+        db.session.commit()
+
+    # Consultando todas informações de todos os sensores
+    @classmethod
+    def list_all_sensors(cls, session):
+        sensors = session.query(cls).all()
+        for sensor in sensors:
+            print(sensor.id, sensor.verdict, sensor.type, sensor.description)
+        return sensors
+
+class SensorData(db.Model):
+    __tablename__ = 'data'
+
+    id = db.Column(db.Integer, ForeignKey('sensor.id'), nullable=False)
+    dateTime = db.Column(db.DateTime, default=datetime.now, primary_key=True)
+    temperature = db.Column(db.Float, nullable=False)
+    # sensor = db.relationship('Sensor',backref='data', lazy=True)  # Adicionando o backref para a relação inversa)
+
+    def __repr__(self):
+        return f'Data {self.id}'
+    
+    # Consultando a temperatura de um sensor
+    @classmethod
+    def find_temperature(cls, session, id):
+        temperature = session.query(cls).filter_by(id=id).all()
+        for data in temperature:
+            print(data.temperature)
+        return temperature
+
+    # Alterando novos valores de dados de um sensor
+    def add_data(cls, id, dateTime, temperature):
+        new_valors = cls(id=id, dateTime=dateTime, temperature=temperature) 
+        db.session.add(new_valors)
+        db.session.commit()
+
 class StatusPlano(db.Model):
+    __tablename__ = 'planos'
+
     id = db.Column(db.Integer, primary_key=True)
     plano = db.Column(db.String(50), nullable=False)
     status = db.Column(db.String(50), nullable=False, default='pending')
+
+    def __repr__(self):
+        return f'Planos {self.id}'
+    
+    @classmethod
+    def list_all_planos(cls, session):
+        planos = session.query(cls).all()
+        for plano in planos:
+            print(plano.id, plano.plano, plano.status)
+        return planos
--- app/planos.py
+++ app/planos.py
@@ -1,43 +1,56 @@
 from .lib.jpmsb import sensor
 from .database import db
-from .models import Sensor, StatusPlano
+from .models import Sensor, StatusPlano, SensorData
+from datetime import datetime
 
 from loguru import logger
 
 
+# Plano de testes de temperatura
 def plano_temperatura(ow_sensor):
+    # app_context.push()
+    ow_sensor = sensor('app/temp/sys/bus/w1/devices')
+    sensors = {}
+ 
+    type = 'sensor1'
+
     # Verifica cada sensor registrado
     sensors_addresses = ow_sensor.list_sensors()
     for index, sensor_address in enumerate(sensors_addresses):
-        logger.debug(sensor_address)
-        logger.debug(ow_sensor.get_temperature(index + 1))
+            dateTime = datetime.now()
+            logger.debug(sensor_address)
+            logger.debug(ow_sensor.get_temperature(index+1))
 
-        temperature = ow_sensor.get_temperature(index + 1)
-        address = sensor_address
+            temperature = ow_sensor.get_temperature(index+1)
+            address = sensor_address
 
-        # Veredito
-        if temperature > 40:
-            verdict = "fail"
-        else:
-            verdict = "pass"
+            # Veredito
+            if temperature > 40:
+                verdict = "fail"
+                description = 'O sensor não passou!'
+            else:
+                verdict = "pass"
+                description = 'O sensor passou!'
 
-        # Salvar o resultado no banco de dados
-        sensor_data = Sensor(
-            serialNumber=address, temperature=temperature, verdict=verdict
-        )
-        db.session.add(sensor_data)
+            Sensor.add_sensor(serialNumber=address, type=type, verdict=verdict, description=description)
+            # sensorx = Sensor(serialNumber=address, type=type, verdict=verdict, description=description)
+            # db.session.add(sensorx)
+            id = Sensor.query.filter_by(id="id").first()
+            SensorData.add_data(id=id, dateTime=dateTime, temperature=temperature)
+            # sensor_data = SensorData(dateTime=dateTime, temperature=temperature)
+            # db.session.add(sensor_data)
 
-        # Encontra status do plano e altera para complete
-        status = StatusPlano.query.filter_by(plano="temperatura").first()
-        status.status = "complete"
-        db.session.add(status)
-
-    # Salva no banco de dados
-    db.session.commit()
+            # Encontra status do plano e altera para complete
+            status = StatusPlano.query.filter_by(plano="temperatura").first()
+            status.status = "complete"
+            db.session.add(status)       
+            
+            # Salva no banco de dados
+            db.session.commit()
 
 
 def plano_curto(ow_sensor):
-    logger.info("Inicio plano de curto")
+    logger.info("Início plano de curto")
     # Encontra status do plano e altera para complete
     status = StatusPlano.query.filter_by(plano="curto").first()
     status.status = "complete"
@@ -48,7 +61,7 @@ def plano_curto(ow_sensor):
 
 
 def plano_pinos(ow_sensor):
-    logger.info("Inicio plano de pinos")
+    logger.info("Início plano de pinos")
     # Encontra status do plano e altera para complete
     status = StatusPlano.query.filter_by(plano="pinos").first()
     status.status = "complete"
