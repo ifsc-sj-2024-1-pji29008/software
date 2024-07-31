0185501315a63345c9678c5d8b6f6ff0b221f1de
Mike <mikeicd@gmail.com>
2024-07-31 19:47:56 -0300

    Implementado novas tabelas e alterações na inicialização da DB

--- app/database.py
+++ app/database.py
@@ -1,16 +1,14 @@
-from .jiga_data import planos_info
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def init_db():
-    from .models import SensorData, Sensor, StatusPlano
+    from .models import SensorData, Sensor, Sistema, Plano, Vereditos
+
     db.create_all()
-    
-    # Verifica e adiciona planos do planos_info
-    for plano_key, plano_info in planos_info.items():
-        if not StatusPlano.query.filter_by(plano=plano_key).first():
-            new_plano = StatusPlano(plano=plano_key, status='pending')
-            db.session.add(new_plano)
-    db.session.commit()
+
+    # Verifica e adiciona status do sistema
+    if not Sistema.query.first():
+        db.session.add(Sistema(status="livre"))
+        db.session.commit()
--- app/models.py
+++ app/models.py
@@ -1,23 +1,49 @@
-# Define your database models here
-
 from datetime import datetime
-from . import db
+from enum import Enum
+
+from .database import db
+
+
+class PlanoNome(Enum):
+    TEMP = "temperatura"
+    CURTO = "curto"
+    PINOS = "pinos"
+
 
 class SensorData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    sensor_position = db.Column(db.Integer, nullable=False)
     temperature = db.Column(db.Float, nullable=False)
-    humidity = db.Column(db.Float, nullable=False)
-    verdict = db.Column(db.String(50), nullable=False)
-    type = db.Column(db.String(50), nullable=False)
-    dateTime = db.Column(db.DateTime, default=datetime.now)
-    idSerial = db.Column(db.String(50), nullable=False)
-    serialNumber = db.Column(db.String(50), nullable=False)    
+    timestamp = db.Column(db.DateTime, default=datetime.now)
+    plano_id = db.Column(db.Integer, db.ForeignKey("plano.id"), nullable=False)
+    sensor_id = db.Column(db.Integer, db.ForeignKey("sensor.id"), nullable=False)
+    sensor = db.relationship("Sensor", backref=db.backref("sensor_data", lazy=True))
+    plano = db.relationship("Plano", backref=db.backref("sensor_data", lazy=True))
+
+
 class Sensor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    temperature = db.Column(db.Float, nullable=False)
-    verdict = db.Column(db.String(50), nullable=False)
-    serialNumber = db.Column(db.String(50), nullable=False) 
-class StatusPlano(db.Model):
+    serialNumber = db.Column(db.String(50), nullable=False, unique=True)
+
+
+class Plano(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    plano = db.Column(db.String(50), nullable=False)
-    status = db.Column(db.String(50), nullable=False, default='pending')
+    nome = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(50), nullable=False, default="esperando")
+    timestamp = db.Column(db.DateTime, default=datetime.now, unique=True)
+
+
+class Vereditos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sensor_id = db.Column(db.Integer, db.ForeignKey("sensor.id"), nullable=False)
+    plano_id = db.Column(db.Integer, db.ForeignKey("plano.id"), nullable=False)
+    resultado = db.Column(db.String(50), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.now)
+    comentario = db.Column(db.Text)
+    sensor = db.relationship("Sensor", backref=db.backref("vereditos", lazy=True))
+    plano = db.relationship("Plano", backref=db.backref("vereditos", lazy=True))
+
+
+class Sistema(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(50), nullable=False, default="livre")
