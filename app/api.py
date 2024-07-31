7779d83a377b7a40b2fe35d9a94cf3511e72211c
Mike <mikeicd@gmail.com>
2024-07-31 19:46:57 -0300

    Implementado recursos da API e alterações na documentação YAML

--- app/api.py
+++ app/api.py
@@ -1,9 +1,9 @@
 import re
-from .models import Sensor, StatusPlano
-from .database import db
+from .models import Plano, Sensor, Sistema, PlanoNome
+from .database import db, init_db
 from .planos import seleciona_plano
 
-from flask import Blueprint, jsonify, request, current_app
+from flask import Blueprint, jsonify, current_app
 from loguru import logger
 from flasgger import swag_from
 
@@ -16,46 +16,113 @@ bp = Blueprint("api", __name__, url_prefix="/api")
 ###    GET   ###
 #################
 
-@bp.route("/execucoes", methods=["GET"])
-@swag_from('swagger_docs/get_execucoes.yaml')
-def get_execucoes():
-    fake = {"plano": "plano1", "timestamp": "2021-01-01 00:00:00", "status": "concluido"}
-    return jsonify(fake)
+
+@bp.route("/planos", methods=["GET"])
+@swag_from("swagger_docs/get_planos.yaml")
+def get_planos():
+    try:
+        planos = Plano.query.all()
+    except Exception as e:
+        logger.error(e)
+        return jsonify({"error": "Plano não encontrado"}), 404
+
+    to_send = []
+    for plano in planos:
+        to_send.append(
+            {
+                "id": plano.id,
+                "nome": plano.nome,
+                "timestamp": plano.timestamp,
+                "status": plano.status,
+            }
+        )
+    return jsonify(to_send)
 
 
-@bp.route("/execucoes/<timestamp>", methods=["GET"])
-@swag_from('swagger_docs/get_execucao.yaml')
-def get_execucao(timestamp):
-    fake = {"plano": "plano1", "sensores": [{"num-serie": "123", "veredito": "ok", "posicao": "1"}]}
-    return jsonify(fake)
+@bp.route("/planos/<id>", methods=["GET"])
+@swag_from("swagger_docs/get_plano.yaml")
+def get_plano(id):
+    try:
+        plano = Plano.query.filter_by(id=id).first()
+    except AttributeError:
+        return jsonify({"error": "Plano não encontrado"}), 404
+
+    to_send = {}
+    to_send["vereditos"] = []
+    for veredito in plano.vereditos:
+        to_send["vereditos"].append(
+            {
+                "id": veredito.id,
+                "sensor": veredito.sensor.serialNumber,
+                "resultado": veredito.resultado,
+                "timestamp": veredito.timestamp,
+            }
+        )
+
+    to_send["dados"] = []
+    for dado in plano.sensor_data:
+        to_send["dados"].append(
+            {
+                "timestamp": dado.timestamp,
+                "posicao": dado.sensor_position,
+                "temperatura": dado.temperature,
+            }
+        )
+    return jsonify(to_send)
 
 
 @bp.route("/sensores", methods=["GET"])
-@swag_from('swagger_docs/get_sensores.yaml')
+@swag_from("swagger_docs/get_sensores.yaml")
 def get_sensores():
-    fake = {"num-serie": "123"}
-    return jsonify(fake)
+    try:
+        sensores = Sensor.query.all()
+    except AttributeError:
+        return jsonify({"error": "Sensores não encontrados"}), 404
+
+    to_send = []
+    for sensor in sensores:
+        to_send.append(sensor.serialNumber)
+    return jsonify(to_send)
 
 
 @bp.route("/sensores/<numserie>", methods=["GET"])
-@swag_from('swagger_docs/get_sensor.yaml')
+@swag_from("swagger_docs/get_sensor.yaml")
 def get_sensor(numserie):
-    fake = {"num-serie": "123", "dados": [{"timestamp": "2021-01-01 00:00:00", "veredito": "ok", "posicao": "1"}]}
-    return jsonify(fake)
 
+    try:
+        sensor = Sensor.query.filter_by(serialNumber=numserie).first()
+    except AttributeError:
+        return jsonify({"error": "Sensor não encontrado"}), 404
 
-@bp.route("/sensores/<numserie>/dados", methods=["GET"])
-@swag_from('swagger_docs/get_sensor_dados.yaml')
-def get_sensor_dados(numserie):
-    fake = {"num-serie": "123", "dados": [{"timestamp": "2021-01-01 00:00:00", "veredito": "ok", "posicao": "1"}]}
-    return jsonify(fake)
+    to_send = {}
+    to_send["dados"] = []
+    to_send["vereditos"] = []
+    for dado in sensor.sensor_data:
+        to_send["dados"].append(
+            {
+                "temperatura": dado.temperature,
+                "timestamp": dado.timestamp,
+                "posicao": dado.sensor_position,
+            }
+        )
+
+    for veredito in sensor.vereditos:
+        to_send["vereditos"].append(
+            {
+                "id": veredito.id,
+                "planos": veredito.plano.nome,
+                "timestamp": veredito.timestamp,
+                "resultado": veredito.resultado,
+            }
+        )
+    return jsonify(to_send)
 
 
 @bp.route("/status", methods=["GET"])
-@swag_from('swagger_docs/get_status.yaml')
+@swag_from("swagger_docs/get_status.yaml")
 def get_stats():
-    fake = {"status": "livre"}
-    return jsonify(fake)
+    sis = Sistema.query.first()
+    return jsonify({"status": sis.status})
 
 
 #################
@@ -64,88 +131,50 @@ def get_stats():
 
 
 @bp.route("/reset", methods=["POST"])
-@swag_from('swagger_docs/post_reset.yaml')
+@swag_from("swagger_docs/post_reset.yaml")
 def reset():
-    pass
+    db.drop_all()
+    init_db()
+    return jsonify({"message": "Banco de dados resetado"}), 200
 
 
-@bp.route("/execucoes", methods=["POST"])
-@swag_from('swagger_docs/post_execucao.yaml')
-def inicia_execucao():
-    req = request.get_json()
-    plano = req.get("plano")
-    if not plano:
-        return jsonify({"error": "Plano não encontrado"}), 400
-    
-    status = StatusPlano.query.first()
-    if status:
-        return jsonify({"error": "Execução em andamento"}), 400
-    
-    status = StatusPlano(plano=plano)
-    db.session.add(status)
-    db.session.commit()
-    
-    thread = threading.Thread(target=seleciona_plano, args=(plano,))
-    thread.start()
-    
-    return jsonify({"message": "Execução iniciada"}), 200
-
-
-
-######################
-### PARA REMOVER   ###
-######################
-
-# Rota com os vereditos dos testes
-@bp.route("/vereditos")
-def get_verdicts():
-    # Coleta os vereditos do banco de dados
-    sensor_data = Sensor.query.all()
-    to_send = []    
-    for sensor in sensor_data:
-        to_send.append(
-            {
-                "serialNumber": sensor.serialNumber,
-                "temperature": sensor.temperature,
-                "verdict": sensor.verdict,
-            }
-        )    
-    return jsonify(to_send)
-
-
-# Simula a coleta do status
-@bp.route("/status/<plano>", methods=["GET"])
-def get_status(plano):
-    # Coleta o status do plano
-    logger.debug(plano)
-    status = StatusPlano.query.filter_by(plano=plano).first()
-    status = status.status
-    print(status)
-    if status == "pending":
-        return jsonify({"status": "pending"})
-    elif status == "complete":
-        return jsonify({"status": "complete"})
-
-
-# Inicia execução do plano
-@bp.route("/iniciar/<plano>", methods=["POST"])
-def iniciar_plano(plano):
-    # Altera o status do plano para pending
-    status = StatusPlano.query.filter_by(plano=plano).first()
-    status.status = "pending"
-    db.session.add(status)
-
+@bp.route("/plano/<nome>", methods=["POST"])
+@swag_from("swagger_docs/post_plano.yaml")
+def inicia_plano(nome):
+    # Verifica se o plano é válido pelo Enum PlanoNome
     try:
-        # Salva no banco de dados
-        db.session.commit()
-        # Inicia o plano
-        threading.Thread(
-            target=seleciona_plano,
-            args=(
-                current_app.app_context(),
-                plano,
-            ),
-        ).start()
-        return jsonify({"status": "pending"})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        plano_nome = PlanoNome(nome)
+    except ValueError:
+        return jsonify({"error": "Plano inválido"}), 400
+
+    sis = Sistema.query.first()
+    if sis.status == "executando":
+        return jsonify({"error": "Execução em andamento"}), 400
+
+    sis.status = "executando"
+    db.session.add(sis)
+
+    pl = Plano(nome=plano_nome.value, status="executando")
+    db.session.add(pl)
+    db.session.commit()
+    plano_id = pl.id
+    thread = threading.Thread(
+        target=seleciona_plano,
+        args=(
+            current_app.app_context(),
+            plano_id,
+        ),
+    )
+    thread.start()
+
+    return (
+        jsonify(
+            {
+                "id": pl.id,
+                "nome": pl.nome,
+                "status": pl.status,
+                "timestamp": pl.timestamp,
+            }
+        ),
+        200,
+    )
